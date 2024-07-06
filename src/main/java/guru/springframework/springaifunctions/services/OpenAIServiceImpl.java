545155deecffc1cb15ab4b861074068283a93695
royjo <royjoydeep348@gmail.com>
2024-07-06 17:17:44 +0530

    Added new code

--- src/main/java/guru/springframework/springaifunctions/services/OpenAIServiceImpl.java
+++ src/main/java/guru/springframework/springaifunctions/services/OpenAIServiceImpl.java
@@ -1,11 +1,20 @@
 package guru.springframework.springaifunctions.services;
 
 
+import guru.springframework.springaifunctions.functions.WeatherServiceFunction;
 import guru.springframework.springaifunctions.model.Answer;
 import guru.springframework.springaifunctions.model.Question;
 import lombok.RequiredArgsConstructor;
-import org.springframework.ai.chat.ChatClient;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.model.function.FunctionCallbackWrapper;
+import org.springframework.ai.openai.OpenAiChatClient;
+import org.springframework.ai.openai.OpenAiChatOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Created by jt, Spring Framework Guru.
@@ -14,10 +23,41 @@ import org.springframework.stereotype.Service;
 @Service
 public class OpenAIServiceImpl implements OpenAIService {
 
-    final ChatClient chatClient;
+    @Value("${sfg.aiapp.apiNinjasKey}")
+    private String apiNinjasKey;
+
+    final OpenAiChatClient openAiChatClient;
 
     @Override
     public Answer getAnswer(Question question) {
-       return new Answer("to-do implement me!");
+        var promptOptions = OpenAiChatOptions.builder()
+                .withFunctionCallbacks(List.of(FunctionCallbackWrapper.builder(new WeatherServiceFunction(apiNinjasKey))
+                        .withName("CurrentWeather")
+                        .withDescription("Get the current weather for a location")
+                        .build()))
+                .build();
+
+        Message userMessage = new PromptTemplate(question.question()).createMessage();
+
+        var response = openAiChatClient.call(new Prompt(List.of(userMessage), promptOptions));
+
+        return new Answer(response.getResult().getOutput().getContent());
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
